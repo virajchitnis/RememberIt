3ecbe10a29584c98f0364eda2138c6e8a9cd04c9
Viraj Chitnis <chitnisviraj@gmail.com>
2015-02-21 14:12:08 -0500

    Login panel complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var packageJSON = require('../package.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { app_name: packageJSON.name });
 });
 
 module.exports = router;
